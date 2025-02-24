fd3720a185cba51aa8904366e4df8010ca0f4b6e
Eche00 <echeeze956@gmail.com>
2025-02-24 03:04:30 +0100

    history set up

---
+++ client/src/pages/History.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function History() {
+  return (
+    <div className=" hidden lg:flex lg:w-[320px] xl:w-[360px] dark:border-l-[1px] dark:border-[#343839]">
+      {/* Container  */}
+      <main>
+        <section className="">1</section>
+        <section className="">2</section>
+        <section className="">3</section>
+      </main>
+    </div>
+  );
+}
+
+export default History;
