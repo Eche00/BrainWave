7c6191e6ad14f302d3ed662eed8049a1d6fc9a28
Eche00 <echeeze956@gmail.com>
2025-03-05 03:19:05 +0100

    sub updates media query completed

--- client/src/pages/updates&faqs/SubUpdates.jsx
+++ client/src/pages/updates&faqs/SubUpdates.jsx
@@ -6,11 +6,11 @@ function SubUpdates() {
   return (
     <div className=" border-t-[1px] border-[#E8ECEF] w-full">
       {/* container  */}
-      <main className="">
+      <main className=" w-full">
         {/* each section  */}
-        <section className=" py-[64px]  flex justify-between">
+        <section className=" py-[64px]  flex justify-between flex-col md:flex-row">
           {/* 1  */}
-          <div className="flex flex-col gap-[20px]">
+          <div className="flex flex-col gap-[20px] ">
             <span>{updateicons.box}</span>
             <p className=" text-[18px] font-[600] leading-[32px] w-[256px]">
               Improved Natural Language Processing (NLP) Algorithms
@@ -20,41 +20,50 @@ function SubUpdates() {
             </p>
           </div>
           {/* 2 */}
-          <div className=" relative w-[600px] h-[504px] rounded-[24px] overflow-hidden">
-            <img src={updatesDummy} className=" w-full object-cover" alt="" />
+          <div className=" relative   overflow-hidden md:w-fit w-full rounded-[24px]">
+            <img
+              src={updatesDummy}
+              className="  object-cover md:w-[600px] sm:w-full w-full lg:h-[528px] md:h-[504px] sm:h-[528px] h-[318px]  rounded-[24px]"
+              alt=""
+            />
             {/* updaate  */}
-            <section className="absolute w-fit right-[5%]  bottom-0 flex flex-col px-[60px] pt-[60px] gap-[24px] rounded-t-[18px] bg-[#FEFEFE] dark:bg-[#141718]">
+            <section className="absolute lg:w-[90%] md:w-[90%] sm:w-[90%] w-[90%] left-[5%]  bottom-0 flex flex-col  sm:px-[60px] px-[32px] sm:pt-[60px] pt-[32px] sm:gap-[24px] gap-[13px] rounded-t-[18px] bg-[#FEFEFE] dark:bg-[#141718]">
               <span className=" absolute right-10 top-10">
                 {updateicons.exit}
               </span>
               <div>
-                <h1 className=" text-[#141718] dark:text-[#FEFEFE] text-[28px] font-[700] leading-[40px]">
+                <h1 className=" text-[#141718] dark:text-[#FEFEFE] sm:text-[28px] font-[700] leading-[40px] text-[15.26px]">
                   Add chat list
                 </h1>
               </div>
               {/* first inputs  */}
-              <div className=" flex gap-[9px]">
+              <div className="flex gap-[9px] overflow-hidden w-full">
                 {/* 1 */}
-                <div className=" text-[#141718] dark:text-[#FEFEFE] text-[14px] font-[600] leading-[24px] flex gap-[6px] flex-col">
-                  <p>Name</p>
-                  <span className=" flex items-center gap-[9px] border-[0.75px] border-[#E8ECEF] dark:border-[1px] dark:border-[#343839] p-[10.5px] rounded-[9px] ">
+                <div className="text-[#141718] dark:text-[#FEFEFE] text-[14px] font-[600] leading-[24px] flex gap-[6px] flex-col w-1/2">
+                  <p className="sm:text-[14px] font-[600] leading-[24px] text-[7.63px]">
+                    Name
+                  </p>
+                  <span className="flex items-center gap-[9px] border-[0.75px] border-[#E8ECEF] dark:border-[1px] dark:border-[#343839] sm:p-[10.5px] p-[5px] rounded-[9px] w-full">
                     {updateicons.message}
                     <input
                       type="text"
                       placeholder="Name"
-                      className=" placeholder:text-[#6C7275BF] flex-1 outline-none"
+                      className="placeholder:text-[#6C7275BF] outline-none w-full placeholder:text-[7.63px]  placeholder:sm:text-[14px] text-[7.63px] sm:text-[14px]"
                     />
                   </span>
                 </div>
-                {/* 2  */}
-                <div className=" text-[#141718] dark:text-[#FEFEFE] text-[14px] font-[600] leading-[24px] flex gap-[6px] flex-col w-[174px]">
-                  <p>Color</p>
-                  <span className=" flex items-center gap-[9px] border-[0.75px] border-[#E8ECEF] dark:border-[1px] dark:border-[#343839] p-[10.5px] rounded-[9px] ">
-                    <span className=" w-[10px] h-[10px] rounded-[0px] bg-[#3E90F0]"></span>
+
+                {/* 2 */}
+                <div className="text-[#141718] dark:text-[#FEFEFE] text-[14px] font-[600] leading-[24px] flex gap-[6px] flex-col w-1/2">
+                  <p className="sm:text-[14px] font-[600] leading-[24px] text-[7.63px]">
+                    Color
+                  </p>
+                  <span className="flex items-center gap-[9px] border-[0.75px] border-[#E8ECEF] dark:border-[1px] dark:border-[#343839] sm:p-[10.5px] p-[5px] rounded-[9px] w-full ">
+                    <span className="w-[10px] h-[10px] rounded-[2px] bg-[#3E90F0]"></span>
                     <input
                       type="text"
-                      placeholder="Name"
-                      className=" placeholder:text-[#6C7275BF] flex-1 outline-none"
+                      placeholder="Color"
+                      className="placeholder:text-[#6C7275BF] outline-none w-full placeholder:text-[7.63px]  placeholder:sm:text-[14px] text-[7.63px] sm:text-[14px]"
                     />
                   </span>
                 </div>
@@ -62,25 +71,31 @@ function SubUpdates() {
 
               {/* second inputs  */}
               <div className="flex flex-col gap-[6px]">
-                <h2 className="text-[#141718] dark:text-[#FEFEFE] text-[14px] font-[600] leading-[24px]">
+                <h2 className="text-[#141718] dark:text-[#FEFEFE] sm:text-[14px] font-[600] leading-[24px] text-[7.63px]">
                   Invite team member
                 </h2>
-                <div className="  p-[15px] flex flex-col gap-[15px] border-[0.75px] border-[#E8ECEF] dark:border-[1px] dark:border-[#343839] rounded-t-[9px] border-b-0 dark:border-b-0">
-                  <span className=" flex items-center gap-[9px] border-[0.75px] border-[#E8ECEF] dark:border-[1px] dark:border-[#343839] p-[10.5px] rounded-[9px] ">
-                    {updateicons.email}
+                <div className="  sm:p-[15px] p-[8.18px] flex flex-col gap-[15px] border-[0.75px] border-[#E8ECEF] dark:border-[1px] dark:border-[#343839] rounded-t-[9px] border-b-0 dark:border-b-0">
+                  <span className=" flex items-center gap-[9px] border-[0.75px] border-[#E8ECEF] dark:border-[1px] dark:border-[#343839] sm:p-[10.5px] p-[5px] rounded-[9px] ">
+                    <span>{updateicons.email}</span>
                     <input
                       type="text"
                       placeholder="Email, comma separated"
-                      className=" placeholder:text-[#6C7275BF] flex-1 outline-none"
+                      className=" placeholder:text-[#6C7275BF] flex-1 outline-none placeholder:text-[7.63px]   placeholder:sm:text-[14px] text-[7.63px] sm:text-[14px] flex  sm:w-full "
                     />
-                    <p className="text-[#141718] dark:text-[#FEFEFE] text-[12px] font-[600] leading-[24px] flex items-center">
-                      Can view <span>&#xf107;</span>
+                    <p className="text-[#141718] dark:text-[#FEFEFE] text-[7.63px] sm:text-[12px] font-[600] leading-[24px] flex items-center gap-[6px] text-nowrap text-ellipsis">
+                      Can view
+                      <span className=" hidden dark:block">
+                        {updateicons.arrowdownlight}{" "}
+                      </span>
+                      <span className=" dark:hidden block">
+                        {updateicons.arrowdowndark}{" "}
+                      </span>
                     </p>
                   </span>
-                  <p className="text-[#6C727580]  text-[12px] font-[500] leading-[20px]">
+                  <p className="text-[#6C727580] text-[6.53px]  sm:text-[12px] font-[500] leading-[20px]">
                     Only people invited in this list can access
                   </p>
-                  <section className=" flex items-center justify-between">
+                  <section className=" hidden sm:flex items-center justify-between">
                     <div className=" flex gap-[9px] items-center">
                       <img
                         src={Avatar}
@@ -91,8 +106,8 @@ function SubUpdates() {
                         Janiya(you)
                       </p>
                     </div>
-                    <p className="text-[#6C727580]  text-[12px] font-[500] leading-[20px]">
-                      Full access
+                    <p className="text-[#6C727580]  text-[12px] font-[500] leading-[20px] flex items-center gap-[6px]">
+                      Full access <span>{updateicons.tick}</span>
                     </p>
                   </section>
                 </div>
