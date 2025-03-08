5e1417b7d071e8bb38f15e0fca082988707cff38
Eche00 <echeeze956@gmail.com>
2025-03-08 02:03:57 +0100

    making container head component to set the search display state

--- client/src/components/Container.jsx
+++ client/src/components/Container.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./navigation/Sidebar";
+import Search from "../pages/Search";
 
 function Container() {
+  const [search, setSearch] = useState(true);
   return (
     <div className=" flex gap-0">
+      {search && <Search setSearch={setSearch} />}
+
       <section className=" hidden sm:flex  bg-[#141718] text-white  lg:w-fit  sm:w-[96px] p-0 m-0 border-none">
-        <Sidebar />
+        <Sidebar setSearch={setSearch} />
       </section>
       <section className=" flex flex-1 dark:bg-black bg-white m-0 p-0 border-none">
         <Outlet />
