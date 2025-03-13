cec5c4b36abd73f0661b930b281e786eb9266725
Eche00 <echeeze956@gmail.com>
2025-03-13 20:56:43 +0100

    container edit for state handling

--- client/src/components/Container.jsx
+++ client/src/components/Container.jsx
@@ -3,18 +3,25 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "./navigation/Sidebar";
 import Search from "../pages/Search";
 import Chatlist from "../pages/Chatlist";
+import Setting from "../pages/Setting";
 
 function Container() {
   const [search, setSearch] = useState(false);
   const [addList, setAddlist] = useState(false);
+  const [settings, setSettings] = useState(false);
 
   return (
     <div className=" flex gap-0">
       {search && <Search setSearch={setSearch} />}
       {addList && <Chatlist setAddlist={setAddlist} />}
+      {settings && <Setting setSettings={setSettings} />}
 
       <section className=" hidden sm:flex  bg-[#141718] text-white  lg:w-fit  sm:w-[96px] p-0 m-0 border-none">
-        <Sidebar setSearch={setSearch} setAddlist={setAddlist} />
+        <Sidebar
+          setSearch={setSearch}
+          setAddlist={setAddlist}
+          setSettings={setSettings}
+        />
       </section>
       <section className=" flex flex-1 dark:bg-black bg-white m-0 p-0 border-none">
         <Outlet />
