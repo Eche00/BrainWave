06dbd10492a7401d90bf7bffa6ec60f248dfc7b6
Eche00 <echeeze956@gmail.com>
2025-03-30 21:43:25 +0100

    code generation page

---
+++ src/components/AIpaths/AIcategories/Codegeneration.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Codegeneration({ handleSwitch }) {
+  return (
+    <div>
+      Codegeneration
+      <button id="nav" onClick={handleSwitch}>
+        exit
+      </button>
+    </div>
+  );
+}
+
+export default Codegeneration;
