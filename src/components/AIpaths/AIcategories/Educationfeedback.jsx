996336ba6ab6878edf85f2814541938e47ca2bf6
Eche00 <echeeze956@gmail.com>
2025-03-30 21:43:38 +0100

    education page

---
+++ src/components/AIpaths/AIcategories/Educationfeedback.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Educationfeedback({ handleSwitch }) {
+  return (
+    <div>
+      Educationfeedback
+      <button id="nav" onClick={handleSwitch}>
+        exit
+      </button>
+    </div>
+  );
+}
+
+export default Educationfeedback;
