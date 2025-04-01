ca81916f16a599a96ced4bdaefba3fc97830acf6
Eche00 <echeeze956@gmail.com>
2025-04-01 23:38:43 +0100

    code head setting

--- src/components/AIpaths/AIcategories/Codegeneration.jsx
+++ src/components/AIpaths/AIcategories/Codegeneration.jsx
@@ -3,7 +3,8 @@ import React from "react";
 function Codegeneration({ handleSwitch }) {
   return (
     <div>
-      Codegeneration
+      <div className="  w-full bg-pink-500">hello</div>
+
       <button id="nav" onClick={handleSwitch}>
         exit
       </button>
